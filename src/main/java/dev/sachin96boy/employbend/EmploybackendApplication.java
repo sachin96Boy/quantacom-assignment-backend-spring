2143d8f20d13453eafccf8a7ba1a23d066f07c86
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-09-24 17:45:51 +0530

    init: spring app with jwt and mongodb

---
+++ src/main/java/dev/sachin96boy/employbend/EmploybackendApplication.java
@@ -0,0 +1,13 @@
+package dev.sachin96boy.employbend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sachin96boy/employbend/EmploybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sachin96boy.employbend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
